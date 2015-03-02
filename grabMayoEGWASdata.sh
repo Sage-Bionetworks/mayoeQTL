4e45bcfc48f57d8f9772b914105ecbb160ec7e73
Ben Logsdon <blogsdon@DHCP149223.FHCRC.ORG>
2015-03-02 15:36:10 -0800

    updated information

--- grabMayoEGWASdata.sh
+++ grabMayoEGWASdata.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-#Script to download MayoLOAD GWAS genotypes, MayoEGWAS gene expression data, and covariates for each data-set
-#In addition, this script synchronizes the sample ids between all the data frames so that they are standard
-#Author: Benjamin A Logsdon (ben.logsdon@sagebase.org)
-
-#REQUIREMENTS:
-#Synapse R client installed (https://www.synapse.org/#!Synapse:syn1834618)
-#Have a synapse account (https://www.synapse.org/#!RegisterAccount:0)
-#Data access approval to MayoLOADGWAS data (https://www.synapse.org/#!Synapse:syn2954402)
-#Data access approval to MayoEGWAS data (https://www.synapse.org/#!Synapse:syn2910255)
 
 echo "Download data from Synapse"
 Rscript grabMayoEGWASdata.R
